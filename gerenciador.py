49a199daffb425eb1e1d7cb5eba76115825ec7b8
Arthur Sanches <Arthursks7@gmail.com>
2024-04-02 22:41:35 -0300

    function to complete the task and function to delete the task

--- gerenciador.py
+++ gerenciador.py
@@ -25,6 +25,20 @@ def atualizar_tarefas(tarefas, indice_tarefa, nova_tarefa):
         print("Indice não existe!")
     return
 
+def completar_tarefa(tarefas, indice_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) -1
+    tarefas[indice_tarefa_ajustado]["completada"] = True
+    print("Tarefa {indice_tarefa} marcada como completada")
+    return
+
+def deletar_tarefas_completadas(tarefas, indice_tarefa):
+    print("Tarefa {indice_tarefa} deletada")
+    for tarefa in tarefas:
+        if tarefa["completada"]:
+            tarefas.remove(tarefa)
+    print("Tarefas deletadas com sucesso!")
+    return
+
 tarefas = []
 
 while True:
@@ -48,7 +62,13 @@ while True:
         indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
         novo_nome = input("Digite a nova tarefa: ")
         atualizar_tarefas(tarefas, indice_tarefa, novo_nome)
-    
+    elif escolha == "4":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja marcar como completada: ")
+        completar_tarefa(tarefas, indice_tarefa)
+    elif escolha == "5":
+        deletar_tarefas_completadas(tarefas)
+        ver_tarefas(tarefas)
     elif escolha == "6":
         break
     
